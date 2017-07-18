11e8d5369c6a69f078e847dfe719658668246f1d
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-18 23:03:36 +0000

    Add url regex for link and github validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 class Project < ApplicationRecord
+  VALID_URL_REGEX = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  
   validates :name,    presence: true, length: { maximum: 35 }
   validates :tagline, presence: true,  length: { maximum: 80 }
   validates :description, presence: true, length: { within: 100..1000 }
-  validates :link, presence: true, length: { maximum: 50 }
-  validates :github, length: { maximum: 50 }
+  validates :link, presence: true, length: { maximum: 50 }, format: { with: VALID_URL_REGEX }
+  validates :github, length: { maximum: 50 }, format: { with: VALID_URL_REGEX }
 end
