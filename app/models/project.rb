bbba48902f2afd378c38b2e1d92f575b06ba289b
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-18 22:56:45 +0000

    Finish project model validations by TDD

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
-  validates :name, presence: true
-  validates :tagline, presence: true
-  validates :description, presence: true
-  validates :link, presence: true
+  validates :name,    presence: true, length: { maximum: 35 }
+  validates :tagline, presence: true,  length: { maximum: 80 }
+  validates :description, presence: true, length: { within: 100..1000 }
+  validates :link, presence: true, length: { maximum: 50 }
+  validates :github, length: { maximum: 50 }
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,7 +4,7 @@ class ProjectTest < ActiveSupport::TestCase
   
   def setup
     @project = Project.new(name: "Spectral", tagline: "Simple recipe site", 
-                           description: "Awesome website for recipes", 
+                           description: "Awesome website for recipes Awesome website for recipes Awesome website for recipes Awesome website for recipes ", 
                            link: "https://spectrall.herokuapp.com", 
                            github: "https://github.com/faisalhassanx/spectral")
   end
@@ -30,6 +30,36 @@ class ProjectTest < ActiveSupport::TestCase
   
   test "link should be present" do
     @project.link = " "
+    assert_not @project.valid?
+  end
+  
+  test "name should not be more than 35 characters" do
+    @project.name = "a" * 36
+    assert_not @project.valid?
+  end
+  
+  test "tagline should not be more than 60 characters" do
+    @project.tagline = "a" * 81
+    assert_not @project.valid?
+  end
+  
+  test "description should be more than 100 characters" do
+    @project.description = "a" * 99
+    assert_not @project.valid?
+  end
+  
+  test "description should be less than 1000 characters" do
+    @project.description = "a" * 1001
+    assert_not @project.valid?
+  end
+  
+  test "link should not be more than 50 characters" do
+    @project.link = "a" * 51
+    assert_not @project.valid?
+  end
+  
+  test "github should not be more than 50 characters" do
+    @project.github = "a" * 51
     assert_not @project.valid?
   end
   
