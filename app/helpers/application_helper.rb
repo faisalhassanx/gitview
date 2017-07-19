ce0b3c345357df750a7d0a8baa66d33462427027
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-19 23:48:48 +0000

    Add nav link for projects, add styles, add gravatar helper and redesign show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(developer, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(developer.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: developer.name, class: "gravatar")
+  end
 end
