27b743fcb23354f5dec1b7c7d382f5fbf23b2721
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-19 01:58:29 +0000

    Remove pages/home route and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-get 'pages/home' => 'pages#home'
 
 root "pages#home"
 
--- test/integration/pages_test.rb
+++ test/integration/pages_test.rb
@@ -5,11 +5,6 @@ class PagesTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   
-  test "should get home" do
-    get pages_home_url
-    assert_response :success
-  end
-  
   test "should get root" do
     get root_url
     assert_response :success
