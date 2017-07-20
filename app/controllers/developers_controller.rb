67b490a204623a255b5bce4e43eee0acf8183cb7
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-20 23:57:29 +0000

    Add create action to register new developers

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,7 +1,38 @@
 class DevelopersController < ApplicationController
   
+  def index
+    @developers = Developer.all
+  end
+  
+  def show
+    @developer = Developer.find(params[:id])
+  end
+  
   def new
     @developer = Developer.new
+  end
+  
+  def create
+    @developer = Developer.new(developer_params)
+    if @developer.save
+      flash[:success] = "Registration is successful"
+      session[:developer_id] = @developer.id
+      redirect_to projects_path
+    else
+      flash[:danger] = "Please correct the following errors and try again"
+      render 'new'
+    end
+  end
+  
+  
+  
+  
+  
+  private
+  
+  def developer_params
+    params.require(:developer).permit(:name, :email, :password, :password_confirmation,
+                                      :website, :github)
   end
   
 end
--- test/integration/developers_signup_test.rb
+++ test/integration/developers_signup_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class DevelopersSignupTest < ActionDispatch::IntegrationTest
+  
   test "should get register path" do
     get register_path
     assert_response :success
   end
+  
 end
