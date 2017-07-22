13c24a16d0d00e12206838f0d93ef07b3444959e
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-22 21:58:14 +0000

    Add sessions for login and log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    developer = Developer.find_by(email: params[:email])
+    if developer && developer.authenticate(params[:password])
+      session[:developer_id] = developer.id
+      flash[:success] = "You have logged in successfully"
+      redirect_to projects_path
+    else
+      flash.now[:danger] = "Invalid email or password"
+      render 'sessions/new'
+    end
+  end
+  
+  def destroy
+    session[:developer_id] = nil
+    flash[:success] = "You have logged out successfully"
+    redirect_to root_path
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   get '/register', to: 'developers#new'
+  get   '/login',    to: "sessions#new"
+  post  '/login',    to: "sessions#create"
+  get   '/logout',   to: "sessions#destroy"
+  
   resources :developers, except: [:new]
   
   resources :projects
