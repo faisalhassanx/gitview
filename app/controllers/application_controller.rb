e0f618df4b605e554b27f539e77ae0c63af9eb77
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-22 22:05:27 +0000

    Add logged_in and current_user methods and add session and profile links to nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    @current_user ||= Developer.find(session[:developer_id]) if session[:developer_id]
+  end
+  
+  def logged_in?
+    !!current_user
+  end
 end
