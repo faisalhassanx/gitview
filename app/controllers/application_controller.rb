b7707951509a1a5f34edc599a2d9162219e2c245
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-22 22:13:34 +0000

    Edit sessions controller and add require user method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :require_user
+  
   
   def current_user
     @current_user ||= Developer.find(session[:developer_id]) if session[:developer_id]
@@ -9,5 +10,12 @@ class ApplicationController < ActionController::Base
   
   def logged_in?
     !!current_user
+  end
+  
+  def require_user
+    if !logged_in?
+      flash[:danger] = "Please log in or sign up"
+      redirect_to login_path
+    end
   end
 end
