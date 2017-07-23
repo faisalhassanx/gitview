9e5f7dac2cfb32e402fee68fa45978b03abe5e33
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-23 23:30:28 +0000

    Generate tables for project types and tech used

---
+++ db/migrate/20170723232834_drop_technologies.rb
@@ -0,0 +1,5 @@
+class DropTechnologies < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :technologies
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723204652) do
+ActiveRecord::Schema.define(version: 20170723232936) do
 
   create_table "developers", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20170723204652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "developer_id"
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
   end
 
 end
