8686b701d3cd228cca461d2b0430cc18c822e9e5
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-23 23:35:16 +0000

    Add validations to type and technology models

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,3 +1,4 @@
 class Technology < ApplicationRecord
-  
+  validates :name, presence: true, length: { in: 2..25 }
+  validates_uniqueness_of :name
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,4 @@
 class Type < ApplicationRecord
-  
+  validates :name, presence: true, length: { in: 6..20 }
+  validates_uniqueness_of :name
 end
