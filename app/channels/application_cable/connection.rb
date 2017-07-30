ae864c603ec499640648dadc97b522851a55105d
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-30 23:23:04 +0000

    Add config for ActionCable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    
+    def connect
+      
+    end
+    
+    def disconnect
+      
+    end
+    
+    def find_current_user
+      
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  mount ActionCable.server => '/cable'
+
   resources :types, only: [:new, :create, :show]
  
- resources :technologies, only: [:new, :create, :show]
+  resources :technologies, only: [:new, :create, :show]
   
   get '/register', to: 'developers#new'
   get   '/login',    to: "sessions#new"
