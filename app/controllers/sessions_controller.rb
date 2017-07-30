c516ccb0afeb8518647f610ce6d579b99fed4266
Faisal Hassan <faisal.hassanx@gmail.com>
2017-07-30 23:27:42 +0000

    Assign cookies to signed in developer id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     developer = Developer.find_by(email: params[:email])
     if developer && developer.authenticate(params[:password])
       session[:developer_id] = developer.id
+      cookies.signed[:developer_id] = developer.id
       flash[:success] = "You have logged in successfully"
       redirect_to projects_path
     else
