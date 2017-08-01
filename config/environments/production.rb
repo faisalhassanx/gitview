fc234f0a32b133f4fa8d9c69b42d7d2daf1a2542
Faisal Hassan <faisal.hassanx@gmail.com>
2017-08-01 23:45:53 +0000

    Add configurations for action cable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.web_socket_server_url = "wss://gitview.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://gitview.herokuapp.com', 'http://gitview.herokuapp.com']
 end
