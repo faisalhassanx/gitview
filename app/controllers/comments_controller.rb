41db42a74df25eb1d04238e4af8c06922d8a4878
Faisal Hassan <faisal.hassanx@gmail.com>
2017-08-01 23:02:21 +0000

    Fix action cable streaming for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,6 @@ class CommentsController < ApplicationController
     @comment.developer = current_user
     if @comment.save
       ActionCable.server.broadcast "comments", render(partial: 'comments/comment', object: @comment )
-      # flash[:success] = "Comment has been successfully created."
-      # redirect_to project_path(@project)
     else
       flash[:danger] = "Please try again"
       redirect_back fallback_location: root_path
