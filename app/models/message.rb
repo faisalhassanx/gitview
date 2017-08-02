097f8348aa4a1c41cedcdf2b400d784d3ea8c1ec
Faisal Hassan <faisal.hassanx@gmail.com>
2017-08-02 23:22:06 +0000

    Restrict the number of messages in the gitchat box

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ApplicationRecord
   
   validates :content, presence: true
   validates :developer_id, presence: true
+  
+  def self.most_recent
+    order(:created_at).last(25)
+  end
 end
