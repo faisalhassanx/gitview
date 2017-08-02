e7422d318e9d919c9d7f4a3ab8f2faf5dd8ffb99
Faisal Hassan <faisal.hassanx@gmail.com>
2017-08-02 01:04:12 +0000

    Add validations and associations between messages and developers

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -14,6 +14,7 @@ class Developer < ApplicationRecord
   
   has_many :projects, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :messages, dependent: :destroy
   
   has_secure_password
   
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+  belongs_to :developer
   
-  
+  validates :content, presence: true
+  validates :developer_id, presence: true
 end
