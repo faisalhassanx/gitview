23cb76759101b12b5560d8f4dc5e11a7812b5a27
Faisal Hassan <faisal.hassanx@gmail.com>
2017-08-02 00:59:47 +0000

    Generate messages table and create message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724065308) do
+ActiveRecord::Schema.define(version: 20170802005732) do
 
   create_table "comments", force: :cascade do |t|
     t.text "description"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20170724065308) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "developer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "project_technologies", force: :cascade do |t|
